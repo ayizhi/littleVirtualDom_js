bf8f80d2e521d8ac9fb0ae4324dc1a1dac860648
yizhi <zhangyiizhi@126.com>
2016-09-20 18:16:47 +0800

    fix

--- list_diff.js
+++ list_diff.js
@@ -1,146 +1,75 @@
+//列表对比找最小步的算法
+
+
 /**
- * Diff two list in O(N).
- * @param {Array} oldList - Original List
- * @param {Array} newList - List After certain insertions, removes, or moves
- * @return {Object} - {moves: <Array>}
- *                  - moves is a list of actions that telling how to remove and insert
- */
-function listDiff (oldList, newList, key) {
-	var oldMap = makeKeyIndexAndFree(oldList, key)
-	var newMap = makeKeyIndexAndFree(newList, key)
+ * @param{Array} oldList - 原list
+ * @param{Array} newList - 新list
+ * @param{string} key - 关键字段名
+ * **/
 
-	var newFree = newMap.free
 
-	var oldKeyIndex = oldMap.keyIndex
-	var newKeyIndex = newMap.keyIndex
-
-	var moves = []
-
-	// a simulate list to manipulate
-	var children = []
-	var i = 0
-	var item
-	var itemKey
-	var freeIndex = 0
-
-	// fist pass to check item in old list: if it's removed or not
-	while (i < oldList.length) {
-		item = oldList[i]
-		itemKey = getItemKey(item, key)
-		if (itemKey) {
-			if (!newKeyIndex.hasOwnProperty(itemKey)) {
-				children.push(null)
-			} else {
-				var newItemIndex = newKeyIndex[itemKey]
-				children.push(newList[newItemIndex])
-			}
-		} else {
-			var freeItem = newFree[freeIndex++]
-			children.push(freeItem || null)
-		}
-		i++
-	}
-
-	var simulateList = children.slice(0)
-	console.log(simulateList)
+var oldList = [{id:'a'},{id:'b'},{id:'c'},{id:'d'},{id:'e'}]
+var newList = [{id:'c'},{id:'a'},{id:'b'},{id:'c'},{id:'f'},{id:'g'}]
 
 
 
-	// remove items no longer exist
-	i = 0
-	while (i < simulateList.length) {
-		if (simulateList[i] === null) {
-			remove(i)
-			removeSimulate(i)
-		} else {
-			i++
-		}
-	}
+function listDiff(oldList,newList,key){
+	var oldMap = buildKeyIndexAndFree(oldList);
+	var newMap = buildKeyIndexAndFree(newList);
 
-	// i is cursor pointing to a item in new list
-	// j is cursor pointing to a item in simulateList
-	var j = i = 0
-	while (i < newList.length) {
-		item = newList[i]
-		itemKey = getItemKey(item, key)
+	var newFree = newMap['freeList'];
 
-		var simulateItem = simulateList[j]
-		var simulateItemKey = getItemKey(simulateItem, key)
+	var oldKeyIndex = oldMap.keyIndex;
+	var newKeyIndex = newMap.keyIndex;
 
-		if (simulateItem) {
-			if (itemKey === simulateItemKey) {
-				j++
-			} else {
-				// new item, just inesrt it
-				if (!oldKeyIndex.hasOwnProperty(itemKey)) {
-					insert(i, item)
-				} else {
-					// if remove current simulateItem make item in right place
-					// then just remove it
-					var nextItemKey = getItemKey(simulateList[j + 1], key)
-					if (nextItemKey === itemKey) {
-						remove(i)
-						removeSimulate(j)
-						j++ // after removing, current j is right, just jump to next one
-					} else {
-						// else insert item
-						insert(i, item)
-					}
+	var moves = [];
+
+	//先对老的list与新的list进行对比,找出哪些被删除掉,并生成新的simulateList
+	var children = [];
+	var item,itemKey,freeIndex = 0;
+	(function(){
+		for(var i = 0,len = oldList.length;i<len;i++){
+			item = oldList[i];
+			itemKey = getItemKey(item,key);
+			if(itemKey){
+				//判断newlist是否也含有此key,有则将newlist中的那一项放入children,没有则置为null,
+				if(newList.hasOwnProperty(itemKey)){
+					var newItemIndex = newKeyIndex[itemKey];
+					children.push(newList[newItemIndex]);
+				}else{
+					children.push(null);
 				}
 			}
-		} else {
-			insert(i, item)
 		}
+	})();
 
-		i++
-	}
-
-	function remove (index) {
-		var move = {index: index, type: 0}
-		moves.push(move)
-	}
-
-	function insert (index, item) {
-		var move = {index: index, item: item, type: 1}
-		moves.push(move)
-	}
-
-	function removeSimulate (index) {
-		simulateList.splice(index, 1)
-	}
-
-	return {
-		moves: moves,
-		children: children
-	}
 }
 
-/**
- * Convert list to key-item keyIndex object.
- * @param {Array} list
- * @param {String|Function} key
- */
-function makeKeyIndexAndFree (list, key) {
-	var keyIndex = {}
-	var free = []
-	for (var i = 0, len = list.length; i < len; i++) {
-		var item = list[i]
-		var itemKey = getItemKey(item, key)
-		if (itemKey) {
-			keyIndex[itemKey] = i
-		} else {
-			free.push(item)
+
+//将位置与key绑定,并且将没有key的自由(free)元素筛出
+function buildKeyIndexAndFree(list,key){
+	var keyIndex = {};
+	var freeList = [];
+	for(var i= 0,len=list.length;i<len;i++){
+		var item = list[i];
+		var itemKey = getItemKey(item,key);
+		if(itemKey){
+			keyIndex[itemKey] = i;
+		}else{
+			freeList.push(item)
 		}
 	}
-	return {
+
+	return{
 		keyIndex: keyIndex,
-		free: free
+		freeList: freeList
 	}
 }
 
-function getItemKey (item, key) {
-	if (!item || !key) return void 666
-	return typeof key === 'string'
-		? item[key]
-		: key(item)
+//知道{k:v}中的k,提取出v
+function getItemKey(item,key){
+	if(!item || !key){
+		return undefined;
+	}
+	return typeof key === 'string' ? item[key] : undefined
 }
